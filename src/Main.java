7198499dc0dd5341637cab8026c6411005f66cef
Gian Lucas Alves Ferreira <gian.ferreira@mega.com.br>
2023-04-09 17:58:25 -0300

    #PROG-6 -  Ajustes

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 import main.java.br.com.vitavault.domain.EnumNivelAcesso;
 import main.java.br.com.vitavault.domain.EnumTipoMovimentacao;
 import main.java.br.com.vitavault.domain.MovimentacaoEstoque;
-import main.java.br.com.vitavault.model.Estoque;
 import main.java.br.com.vitavault.model.Funcionario;
-import main.java.br.com.vitavault.model.ItemEstoque;
 import main.java.br.com.vitavault.model.Papel;
 import main.java.br.com.vitavault.model.Produto;
 import main.java.br.com.vitavault.model.ProdutoNaoDepreciavel;
@@ -14,27 +12,36 @@ import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) {
-        Produto produto = new ProdutoNaoDepreciavel(UUID.randomUUID(), 1, "Teste",
+
+        //cria o papel
+        Papel papel = criarPapel();
+        //cria o funcionario
+        Funcionario funcionarioGian = criarFuncionario(papel);
+        //cria o produto
+        Produto produto = criarProduto();
+
+        //Adiciona o primeiro item o Estoque
+        //ItemEstoque itemTesteEstoque = new ItemEstoque(UUID.randomUUID(), produto, LocalDate.now(), 10L, EnumTipoMovimentacao.ENTRADA);
+
+
+        MovimentacaoEstoque movimentarEntrada = new MovimentacaoEstoque(produto, funcionarioGian, papel, LocalDate.now(), EnumTipoMovimentacao.ENTRADA, 10L);
+
+        movimentarEntrada.movimentarEntrada(movimentarEntrada.getItem().getProduto(), movimentarEntrada.getItem(), movimentarEntrada.getQuantidade());
+
+
+    }
+
+    private static Produto criarProduto() {
+        return new ProdutoNaoDepreciavel(UUID.randomUUID(), 1, "Teste",
                 "Produto criado para teste", new BigDecimal("100.00"), "Dentista", true);
+    }
 
-        Papel papel = new Papel(UUID.randomUUID(), EnumNivelAcesso.DIGITADOR, "Teste");
-        Funcionario funcionarioGian = new Funcionario(UUID.randomUUID(), "Gian", "999", "email@teste",
+    private static Funcionario criarFuncionario(Papel papel) {
+        return new Funcionario(UUID.randomUUID(), "Gian", "999", "email@teste",
                 "End teste", "senha1", papel);
+    }
 
-
-        ItemEstoque itemTesteEstoque = new ItemEstoque(UUID.randomUUID(), produto, null, LocalDate.now(), 10L, EnumTipoMovimentacao.ENTRADA);
-
-
-        Estoque estoque = new Estoque();
-        estoque.adicionarProduto(itemTesteEstoque.getProduto().getId(), itemTesteEstoque);
-
-        itemTesteEstoque.setEstoque(estoque);
-
-
-        MovimentacaoEstoque movimentarEntrada =
-                new MovimentacaoEstoque(UUID.randomUUID(), itemTesteEstoque, funcionarioGian, papel, LocalDate.now(), EnumTipoMovimentacao.ENTRADA, 10L);
-
-        movimentarEntrada.movimentarEntrada(itemTesteEstoque.getEstoque(), itemTesteEstoque.getProduto(), itemTesteEstoque);
-
+    private static Papel criarPapel() {
+        return new Papel(UUID.randomUUID(), EnumNivelAcesso.DIGITADOR, "Teste");
     }
 }
