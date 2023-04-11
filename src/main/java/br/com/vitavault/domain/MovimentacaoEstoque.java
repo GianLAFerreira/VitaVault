b4e0d95ba37f5f27ca764f8e2e45712ff62945f7
Gian Lucas Alves Ferreira <gian.ferreira@mega.com.br>
2023-04-10 22:18:36 -0300

    Revert "#PROG-14 - Ajusta o projeto para trabalhar com a injeção de dependencia"

--- src/main/java/br/com/vitavault/domain/MovimentacaoEstoque.java
+++ src/main/java/br/com/vitavault/domain/MovimentacaoEstoque.java
@@ -17,19 +17,12 @@ public class MovimentacaoEstoque {
     private LocalDate dataMovimentacao;
     private EnumTipoMovimentacao tipoMovimentacao;
     private Long quantidade;
-<<<<<<< Updated upstream
     // private Estoque estoque;
-=======
-
-    // private Estoque estoque;
-    private final Estoque estoque;
->>>>>>> Stashed changes
 
     /*
         Autora: Ariadne Cavilha Jorge
     */
-    private MovimentacaoEstoque(ItemEstoque item, Funcionario funcionario, Papel alcada, LocalDate dataMovimentacao, EnumTipoMovimentacao tipoMovimentacao, Long quantidade, Estoque estoque) {
-        this.estoque = estoque;
+    public MovimentacaoEstoque(ItemEstoque item, Funcionario funcionario, Papel alcada, LocalDate dataMovimentacao, EnumTipoMovimentacao tipoMovimentacao, Long quantidade) {
         this.id = UUID.randomUUID();
         this.item = item;
         this.funcionario = funcionario;
@@ -39,13 +32,8 @@ public class MovimentacaoEstoque {
         this.quantidade = quantidade;
     }
 
-<<<<<<< Updated upstream
     public MovimentacaoEstoque(Produto produto, Funcionario funcionario, Papel alcada, LocalDate dataMovimentacao, EnumTipoMovimentacao tipoMovimentacao, Long quantidade) {
         this(new ItemEstoque(UUID.randomUUID(), produto, LocalDate.now(), 0L, tipoMovimentacao), funcionario, alcada, dataMovimentacao, tipoMovimentacao, quantidade);
-=======
-    public MovimentacaoEstoque(Produto produto, Funcionario funcionario, Papel alcada, LocalDate dataMovimentacao, EnumTipoMovimentacao tipoMovimentacao, Long quantidade, Estoque estoque) {
-        this(new ItemEstoque(UUID.randomUUID(), produto, LocalDate.now(), 0L, tipoMovimentacao), funcionario, alcada, dataMovimentacao, tipoMovimentacao, quantidade, estoque);
->>>>>>> Stashed changes
     }
 
     /*
@@ -61,13 +49,8 @@ public class MovimentacaoEstoque {
         }
     }
 
-<<<<<<< Updated upstream
     public void movimentarEntrada(ItemEstoque itemEstoque, Long quantidade, Estoque estoque) {
         itemEstoque = estoque.vincularEstoqueAoItem(itemEstoque, estoque);
-=======
-    public void movimentarEntrada(ItemEstoque itemEstoque, Long quantidade) {
-        itemEstoque = estoque.vincularEstoqueAoItem(itemEstoque, this.estoque);
->>>>>>> Stashed changes
 
         vincularMovimentacaoAoItem(itemEstoque);
         setarQuantidade(itemEstoque, quantidade);
@@ -97,21 +80,10 @@ public class MovimentacaoEstoque {
         return item;
     }
 
-<<<<<<< Updated upstream
 //    public void setEstoque(Estoque estoque) {
 //        this.estoque = estoque;
 //    }
 
-=======
-    public Estoque getEstoque() {
-        return estoque;
-    }
-
-//    public void setEstoque(Estoque estoque) {
-//        this.estoque = estoque;
-//    }
-
->>>>>>> Stashed changes
 //    public void listarProdutosEstoque() {
 //        estoque.listarProdutosEstoque();
 //    }
